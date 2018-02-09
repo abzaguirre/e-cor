48674f731f9e006454bd45a77179aa9e73e3becc
abzaguirre <32188141+abzaguirre@users.noreply.github.com>
2018-02-09 09:50:26 +0800

    Main

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -1,11 +1,20 @@
 <?php
+
 class Main extends CI_Controller {
+
     function __construct() {
         parent::__construct();
     }
-    
+
     public function index() {
         //DITO CONTROLLER NG MAIN
+        $data = array(
+            'title' => 'E-Cor | Event Coordinator',
+            'wholeUrl' => base_url(uri_string()),
+        );
+        $this->load->view("main/includes/header", $data);
+        $this->load->view("main/main");
+        $this->load->view("main/includes/footer");
     }
-    
+
 }
---
+++ application/views/main/includes/header.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en-US" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!--  
+        Document Title
+        =============================================
+        -->
+        <title>Titan | Multipurpose HTML5 Template</title>
+        <!--  
+        Favicons
+        =============================================
+        -->
+        <link rel="apple-touch-icon" sizes="57x57" href="<?= base_url() ?>assets/main/images/favicons/apple-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="<?= base_url() ?>assets/main/images/favicons/apple-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="<?= base_url() ?>assets/main/images/favicons/apple-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="<?= base_url() ?>assets/main/images/favicons/apple-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="<?= base_url() ?>assets/main/images/favicons/apple-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="<?= base_url() ?>assets/main/images/favicons/apple-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="<?= base_url() ?>assets/main/images/favicons/apple-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="<?= base_url() ?>assets/main/images/favicons/apple-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="<?= base_url() ?>assets/main/images/favicons/apple-icon-180x180.png">
+        <link rel="icon" type="image/png" sizes="192x192" href="<?= base_url() ?>assets/main/images/favicons/android-icon-192x192.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="<?= base_url() ?>assets/main/images/favicons/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="<?= base_url() ?>assets/main/images/favicons/favicon-96x96.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="<?= base_url() ?>assets/main/images/favicons/favicon-16x16.png">
+        <link rel="manifest" href="/manifest.json">
+        <meta name="msapplication-TileColor" content="#ffffff">
+        <meta name="msapplication-TileImage" content="<?= base_url() ?>assets/main/images/favicons/ms-icon-144x144.png">
+        <meta name="theme-color" content="#ffffff">
+        <!--  
+        Stylesheets
+        =============================================
+        
+        -->
+        <!-- Default stylesheets-->
+        <link href="<?= base_url() ?>assets/main/lib/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+        <!-- Template specific stylesheets-->
+        <link href="<?= base_url() ?>https://fonts.googleapis.com/css?family=Roboto+Condensed:400,700" rel="stylesheet">
+        <link href="<?= base_url() ?>https://fonts.googleapis.com/css?family=Volkhov:400i" rel="stylesheet">
+        <link href="<?= base_url() ?>https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
+        <link href="<?= base_url() ?>assets/main/lib/animate.css/animate.css" rel="stylesheet">
+        <link href="<?= base_url() ?>assets/main/lib/components-font-awesome/css/font-awesome.min.css" rel="stylesheet">
+        <link href="<?= base_url() ?>assets/main/lib/et-line-font/et-line-font.css" rel="stylesheet">
+        <link href="<?= base_url() ?>assets/main/lib/flexslider/flexslider.css" rel="stylesheet">
+        <link href="<?= base_url() ?>assets/main/lib/owl.carousel/dist/assets/owl.carousel.min.css" rel="stylesheet">
+        <link href="<?= base_url() ?>assets/main/lib/owl.carousel/dist/assets/owl.theme.default.min.css" rel="stylesheet">
+        <link href="<?= base_url() ?>assets/main/lib/magnific-popup/dist/magnific-popup.css" rel="stylesheet">
+        <link href="<?= base_url() ?>assets/main/lib/simple-text-rotator/simpletextrotator.css" rel="stylesheet">
+        <!-- Main stylesheet and color file-->
+        <link href="<?= base_url() ?>assets/main/css/style.css" rel="stylesheet">
+        <link id="color-scheme" href="<?= base_url() ?>assets/main/css/colors/default.css" rel="stylesheet">
+    </head>
