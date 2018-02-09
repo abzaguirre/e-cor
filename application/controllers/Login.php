f83716dc70177600bbc2efe8152304a1859f45a8
abzaguirre <32188141+abzaguirre@users.noreply.github.com>
2018-02-09 09:16:05 +0800

    Initial commit

---
+++ application/controllers/Login.php
@@ -0,0 +1,12 @@
+<?php
+
+class Login extends CI_Controller {
+    function __construct() {
+        parent::__construct();
+    }
+    
+    public function index() {
+        //DITO LOGIN
+    }
+    
+}
---
+++ application/controllers/Main.php
@@ -0,0 +1,11 @@
+<?php
+class Main extends CI_Controller {
+    function __construct() {
+        parent::__construct();
+    }
+    
+    public function index() {
+        //DITO CONTROLLER NG MAIN
+    }
+    
+}
