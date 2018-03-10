5d99158a516e9d2945545f67ab61adcee5aaba03
Juan Carlo Valencia <carlo.valencia066@gmail.com>
2018-03-10 19:44:48 +0800

    Pending Requests

---
+++ application/controllers/Pending_request.php
@@ -0,0 +1,40 @@
+<?php
+
+class Pending_request extends CI_Controller {
+    function __construct() {
+        parent::__construct();
+        if ($this->session->has_userdata('isloggedin') == FALSE) {
+            //user is not yet logged in
+            $this->session->set_flashdata("err_4", "Login First!");
+            redirect(base_url() . 'login/');
+        } else {
+            $current_user = $this->session->userdata("current_user");
+            if ($this->session->userdata("user_access") == "Client") {
+                //USER!
+                $this->session->set_flashdata("err_5", "You are currently logged in as " . $current_user->client_firstname . " " . $current_user->client_lastname);
+                redirect(base_url() . "Client");
+            } else if ($this->session->userdata("user_access") == "admin") {
+                //ADMIN!
+                //Do nothing!
+            }
+        }
+    }
+    public function index(){
+        $pending_request = $this->Transaction_model->get_pending_transactions($this->session->userdata("userid"));
+
+        $current_ep = $this->Eventplanner_model->get_ep(array("event_planner_id" => $this->session->userdata("userid")))[0];
+        $data = array(
+            "pending_request" => $pending_request,
+            //-- NAV INFO --
+            "title" => $current_ep->event_planner_username,
+            "current_ep" => $current_ep,
+            "ep_username" => "$current_ep->event_planner_username",
+            "ep_picture" => "$current_ep->event_planner_picture"
+        );
+        $this->load->view("event_planner/includes/header", $data);
+        $this->load->view("event_planner/navigation/nav_header");
+        $this->load->view("event_planner/navigation/nav_side");
+        $this->load->view("event_planner/pending_request/main");
+        $this->load->view("event_planner/includes/footer");
+    }
+}
