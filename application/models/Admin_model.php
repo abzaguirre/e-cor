36bbda6b30808ae32268a47facd0751360d0c2ee
Juan Carlo Valencia <carlo.valencia066@gmail.com>
2018-03-16 18:04:41 +0800

    Android Added

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -19,7 +19,15 @@ class Admin_model extends CI_Model {
         $query = $this->db->get($table);
         return ($query->num_rows() > 0 ) ? $query->result() : FALSE;
     }
-
+    
+    public function payment($transaction_id){
+        $table = "transaction";
+        $data = array("transaction_isPaid" => 1);
+        $this->db->where(array("transaction_id" => $transaction_id));
+        $this->db->update($table, $data);
+        return $this->db->affected_rows();
+    }
+    
     public function singleinsert($table, $data) {
         $this->db->insert($table, $data);
         return $this->db->affected_rows();
