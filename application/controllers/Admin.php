485eb2ff82accfb3c5b60fdd886e1cbd60f5240f
Juan Carlo Valencia <carlo.valencia066@gmail.com>
2018-03-16 23:42:19 +0800

    Admin Not Done

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -31,7 +31,6 @@ class Admin extends CI_Controller {
     public function transactions_payment(){
         $current_admin = $this->Admin_model->get_admin(array("admin_id" => $this->session->userdata("userid")))[0];
         $unpaid_transactions = $this->Transaction_model->get_unpaid_transactions();
-        
         $data = array(
             "unpaid_transactions" => $unpaid_transactions,
             //-- NAV INFO --
@@ -68,6 +67,26 @@ class Admin extends CI_Controller {
         $this->load->view("admin/includes/footer");
     }
     
+    public function registration_fee(){
+        $current_admin = $this->Admin_model->get_admin(array("admin_id" => $this->session->userdata("userid")))[0];
+        $unpaid_ep = $this->Admin_model->get_unpaid_ep();
+        
+        $data = array(
+            "unpaid_ep" => $unpaid_ep,
+            //-- NAV INFO --
+            "title" => "E-Cor | $current_admin->admin_username",
+            "current_ep" => $current_admin,
+            "admin_username" => "$current_admin->admin_username",
+            "admin_picture" => "$current_admin->admin_picture"
+        );
+
+        $this->load->view("admin/includes/header", $data);
+        $this->load->view("admin/navigation/nav_header");
+        $this->load->view("admin/navigation/nav_side");
+        $this->load->view("admin/registration_fee/main");
+        $this->load->view("admin/includes/footer");
+    }
+    
     public function show_transaction_exec(){
         $this->session->set_userdata("transaction_id",$this->uri->segment(3));
         redirect(base_url()."admin/show_transaction");
@@ -81,7 +100,7 @@ class Admin extends CI_Controller {
         $data = array(
             "fetched_transaction" => $transaction,
             //-- NAV INFO --
-            "title" => $admin->admin_username,
+            "title" => "E-Cor | $admin->admin_username",
             "current_ep" => $admin,
             "admin_username" => "$admin->admin_username",
             "admin_picture" => "$admin->admin_picture"
@@ -102,6 +121,21 @@ class Admin extends CI_Controller {
     public function payment(){
         $transaction_id = $this->session->userdata("transaction_id");
         $this->Admin_model->payment($transaction_id);
+        $this->session->set_flashdata("show_flash_success", "Transaction is now paid.");
+        redirect(base_url()."Admin/transactions_payment");
+    }
+    
+    public function payment_by_admin_exec(){
+        $this->session->set_userdata("transaction_id",$this->uri->segment(3));
+        $this->session->set_userdata("amount",$this->uri->segment(4));
+        redirect(base_url()."admin/payment_by_admin");
+    }
+    
+    public function payment_by_admin(){
+        $transaction_id = $this->session->userdata("transaction_id");
+        $amount = $this->session->userdata("amount");
+        $this->Admin_model->payment_by_admin($transaction_id);
+        $this->Admin_model->edit_net_income($this->session->userdata("userid"), $amount);
         $this->session->set_flashdata("show_flash_success", "Transaction is now paid.");
         redirect(base_url()."Admin/transactions_payment");
     }
